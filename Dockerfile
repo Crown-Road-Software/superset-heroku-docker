c6e2d0966d2ac727e19809b9f7bdb8bd86786973
Edgars Jēkabsons <edgars@crownroadsoftware.com>
2024-10-02 13:50:07 +0300

    The rest Google Auth setup

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM apache/superset
 # Switching to root to install the required packages
 USER root
 # Install the Postgres driver to connect to the metadata database
-RUN pip install psycopg2-binary
+RUN pip install psycopg2-binary Authlib
 # Switching back to using the `superset` user
 USER superset
 # Add configuration
--- superset_config.py
+++ superset_config.py
@@ -1,4 +1,5 @@
 import os
+from flask_appbuilder.security.manager import AUTH_OAUTH
 #---------------------------------------------------------
 # Superset specific config
 #---------------------------------------------------------
@@ -22,6 +23,25 @@ SECRET_KEY = os.environ['SECRET_KEY']
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 AUTH_TYPE = AUTH_OAUTH
+AUTH_USER_REGISTRATION = True
+AUTH_USER_REGISTRATION_ROLE = "Public"
+OAUTH_PROVIDERS = [
+    {
+        "name": "google",
+        "icon": "fa-google",
+        "token_key": "access_token",
+        "remote_app": {
+            "client_id": os.environ['GOOGLE_CLIENT_ID'],
+            "client_secret": os.environ['GOOGLE_CLIENT_SECRET'],
+            "api_base_url": "https://www.googleapis.com/oauth2/v2/",
+            "client_kwargs": {"scope": "email profile"},
+            "request_token_url": None,
+            "access_token_url": "https://accounts.google.com/o/oauth2/token",
+            "authorize_url": "https://accounts.google.com/o/oauth2/auth",
+            "authorize_params": {"hd": "crownroadsoftware.com"}
+        },
+    }
+]
 
 # Flask-WTF flag for CSRF
 WTF_CSRF_ENABLED = CSRF_ENABLED = True
